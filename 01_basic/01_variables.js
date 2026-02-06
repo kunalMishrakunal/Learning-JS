185164be25673493537f28758f6bb2702b6320c5
Kunal Mishra <knlkunal242@gmail.com>
2026-02-06 17:27:22 +0000

    vari and datatype

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,8 @@
+const account_id = 144553
+let account_email = "Kunal@gmail.com"
+var account_password = "12345"
+account_city = "Jaipur"
+
+
+
+console.table([account_id, account_email, account_password, account_city])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,3 @@
+"use strict"
+
+//alert(3+3)//We are using nodejs, not browser
